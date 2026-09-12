99b30f21cfc0eb191daf1a64225c8f3cd3d30208
ahsaneey <ahsanamfiroz@gmail.com>
2026-09-12 10:01:03 +0530

    Add JWT authentication

---
+++ Controllers/UserController.cs
@@ -0,0 +1,22 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Calligraphy.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UserController : ControllerBase
+{
+    [Authorize]
+    [HttpGet("profile")]
+    public IActionResult GetProfile()
+    {
+        var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+        return Ok(new
+        {
+            UserId = userId
+        });
+    }
+}
